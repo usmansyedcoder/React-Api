a3864ffcf9c388234e24e20e10b6f0f3a32ee797
usmansyedcoder <usmansyedislamian@gmail.com>
2024-06-25 17:55:59 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+import "./App.css";
+import Api from "./Components/Api";
+
+function App() {
+  return <Api />;
+}
+
+export default App;
---
+++ src/Components/Api.jsx
@@ -0,0 +1,28 @@
+import React, { useState, useEffect } from "react";
+import ReactDOM from "react-dom";
+import axios from "axios";
+
+function Api() {
+  const [posts, setPosts] = useState([]);
+
+  React.useEffect(() => {
+    axios.get(`https://www.reddit.com/r/reactjs.json`).then((res) => {
+      const newPosts = res.data.data.children.map((obj) => obj.data);
+
+      setPosts(newPosts);
+    });
+  }, []);
+
+  return (
+    <div>
+      <h1>/r/reactjs</h1>
+      <ul>
+        {posts.map((post) => (
+          <li key={post.id}>{post.title}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default Api;
